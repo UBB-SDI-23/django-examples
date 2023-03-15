376304a016064160ab11765c7de79a752cb6a103
Vlad-Sebastian Ionescu <ionescu.vlad1@gmail.com>
2023-03-15 23:56:40 +0200

    Add ALLOWED_HOSTS all

--- django_example/settings.py
+++ django_example/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-je8s$yryeej)l++hrv=q1dg$69#b$xfil-o$(n7eksot)+n18!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
